90553b4d6431935ee6e2b398396ac84f9dfcafd7
Kasper <kasperbak89@pm.me>
2022-11-08 18:31:13 +0100

    Display a coded version of the secret_word ('_' for every letter)

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,18 +2,41 @@ require 'open-uri'
 
 puts "Hangman Initialized"
 
-dictionary = File.open('words', 'r')
+# Algo Hangman:
+# Make an array of the alphabet
+# display the empty slots of the word ex: 'five' = _ _ _ _ _
+# display a counter of the guesses left
+# Ask the user to pick a letter
+# Update the display if the letter is present in the secret word
+# Update the counter if not
 
-words_array = dictionary.readlines
 
-wanted_length_words = words_array.select {|word| word.chomp.length > 4 && word.chomp.length < 13}
+class Hangman
 
-wanted_length_words.each do |word|
-  puts word.chomp.length
+  DICTIONARY = File.open('words', 'r')
+  WANTED_LENGTH_WORDS = DICTIONARY.readlines.select {|word| word.chomp.length > 4 && word.chomp.length < 13}
+
+  def initialize(guesses)
+    @guesses = guesses
+    @secret_word = WANTED_LENGTH_WORDS.sample.chomp
+    @right_letter = []
+
+  end
+
+  def display_guesses_left
+    puts "You can still make #{@guesses} incorrect guesses before the game ends"
+  end
+
+  def display_coded_word
+    coded_array = @secret_word.split('').map do |letter|
+      '_ '
+    end
+    p coded_array.join
+  end
 end
 
-secret_word = wanted_length_words.sample.chomp
 
-p secret_word
+game = Hangman.new(3)
 
+game.display_coded_word
 
