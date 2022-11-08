78d96ddd4dd29361f693e4c63a5c38ee58bcdf24
Kasper <kasperbak89@pm.me>
2022-11-08 23:16:38 +0100

    Refactor to make saving the game possible

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+require 'yaml'
 
 puts "Hangman Initialized"
 
@@ -10,38 +11,33 @@ puts "Hangman Initialized"
 # Check if that letter is present in the secret code
 # If it is then modify the display to show where that letter appears
 
-
 class Hangman
 
-  DICTIONARY = File.open('words', 'r')
-  WANTED_LENGTH_WORDS = DICTIONARY.readlines.select {|word| word.chomp.length > 4 && word.chomp.length < 13}
-
   attr_accessor :guesses_left
-  attr_reader :secret_word
+  attr_reader :secret_word, :coded_array
 
-  def initialize(guesses)
-    @guesses_left = guesses
-    @secret_word = WANTED_LENGTH_WORDS.sample.chomp
-    @coded_array = []
+  def initialize(guesses_left, secret_word, coded_array, right_letters, wrong_letters)
+    @guesses_left = guesses_left
+    @secret_word = secret_word
+    @coded_array = coded_array
     @right_letters = []
     @wrong_letters = []
     @current_letter = nil
   end
 
   def display
+    puts
     puts "You can still make #{@guesses_left} incorrect guesses before the game ends"
     puts "The letters you guessed that are NOT in the secret word are: #{@wrong_letters}"
     puts "The secret word is"
     puts  @coded_array.join
+    puts
   end
 
-  def code_secret_word
-    @coded_array = @secret_word.split('').map {|letter| '_ '}
-  end
-
-  def get_user_input
+  def deal_with_user_input
     puts "Please type a letter"
-    @current_letter = gets.chomp.downcase
+    current_letter = gets.chomp.downcase
+    @secret_word.include?(current_letter) ? @right_letters << current_letter : @wrong_letters << current_letter and @guesses_left -=1
   end
 
   def deal_with_input_letters
@@ -72,19 +68,46 @@ class Hangman
   def defeat_message
     puts "Too bad you just got hanged before cracking the secret word :("
   end
+
+  def guess
+    until guesses_left == 0 || victory
+      p @secret_word
+      display
+      deal_with_user_input
+      update_coded_array
+    end
+  end
+
+  def to_yaml
+    YAML.dump ({
+      :guesses_left => @guesses_left,
+      :secret_word => @secret_word,
+      :coded_array => @coded_array,
+      :right_letters => @right_letters,
+      :wrong_letters => @wrong_letters,
+      :current_letter => @current_letter
+    })
+  end
+
+  def self.from_yaml(string)
+    data = YAML.load string
+    p data
+    self.new(data[:guesses_left], data[:secret_word], data[:coded_array], data[:right_letters], data[:wrong_letters], data[:current_letter])
+  end
 end
 
+dictionary = File.open('words', 'r')
+words_with_wanted_length = dictionary.readlines.select {|word| word.chomp.length > 4 && word.chomp.length < 13}
+secret_word = words_with_wanted_length.sample.chomp
+coded_array = secret_word.split('').map { |letter| '_ ' }
 
-game = Hangman.new(2)
-game.code_secret_word
+game = Hangman.new(2, secret_word, coded_array, Array.new(), Array.new())
 
-until game.guesses_left == 0 || game.victory
-  p game.secret_word
-  game.display
-  game.get_user_input
-  game.deal_with_input_letters
-  game.update_coded_array
-end
+puts "Do you want to make a guess or save the game ?"
+choice = gets.chomp
+choice == 'guess' ? game.guess : game.to_yaml
 
-game.display
+
+puts
 game.victory ? game.victory_message : game.defeat_message
+p game.coded_array.join
