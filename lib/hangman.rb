cbf413b9ac2f836d56c72c036ba83b93f5a55606
Kasper <kasperbak89@pm.me>
2022-11-08 18:11:20 +0100

    Select a random word from the dictionary of wanted length

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,7 +4,16 @@ puts "Hangman Initialized"
 
 dictionary = File.open('words', 'r')
 
-random_word = dictionary.readlines.sample.chomp
+words_array = dictionary.readlines
 
-p random_word
+wanted_length_words = words_array.select {|word| word.chomp.length > 4 && word.chomp.length < 13}
+
+wanted_length_words.each do |word|
+  puts word.chomp.length
+end
+
+secret_word = wanted_length_words.sample.chomp
+
+p secret_word
+
 
